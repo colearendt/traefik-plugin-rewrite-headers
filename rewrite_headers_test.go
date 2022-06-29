aaf4523c066c026a0b7d558c487e5b7e1f080e72
Cole Arendt <cole.arendt@outlook.com>
2022-06-29 13:01:39 -0400

    add tests for websockets using gorllia/websocket and wstest
    wstest simplifies testing so you do not have to listen on a port to test websockets. This unfortunately adds dependencies for the library

--- rewrite_headers_test.go
+++ rewrite_headers_test.go
@@ -3,8 +3,12 @@ package traefik_plugin_rewrite_headers
 
 import (
 	"context"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/posener/wstest"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -61,6 +65,21 @@ func TestServeHTTP(t *testing.T) {
 						rw.Header().Add(k, h)
 					}
 				}
+				// handle websocket upgrade requests
+				if strings.HasSuffix(req.URL.Path, "/ws") {
+					upgrader := websocket.Upgrader{
+						ReadBufferSize:  1024,
+						WriteBufferSize: 1024,
+					}
+					conn, err := upgrader.Upgrade(rw, req, nil)
+					if err != nil {
+						fmt.Printf("Error in websocket upgrade: %v\n", err)
+					}
+
+					_, _, _ = conn.ReadMessage()
+					fmt.Printf("Done upgrading\n")
+					return
+				}
 				rw.WriteHeader(http.StatusOK)
 			}
 
@@ -73,15 +92,43 @@ func TestServeHTTP(t *testing.T) {
 			req := httptest.NewRequest(http.MethodGet, "/", nil)
 
 			rewriteBody.ServeHTTP(recorder, req)
+
+			// test websocket too
+			resp := testWebsocket(t, rewriteBody)
+
 			for k, expected := range test.expRespHeader {
 				values := recorder.Header().Values(k)
 
 				if !testEq(values, expected) {
-					t.Errorf("Slice arent equals: expect: %+v, result: %+v", expected, values)
+					t.Errorf("Slices are not equal: expect: %+v, result: %+v", expected, values)
+				}
+
+				if resp.StatusCode != http.StatusSwitchingProtocols {
+					t.Errorf("Websocket upgrade error")
 				}
 			}
 		})
 	}
+}
+
+func testWebsocket(t *testing.T, h http.Handler) *http.Response {
+	d := wstest.NewDialer(h)
+	c, resp, err := d.Dial("ws://host/ws", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = c.WriteJSON("test socket")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// close the connection
+	err = c.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return resp
 }
 
 func testEq(a, b []string) bool {
